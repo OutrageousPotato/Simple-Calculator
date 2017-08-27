4d4287ee1a387722c3e027934375bd0eb26d2a82
OutrageousPotato <jonathan@gibsonnotes.com>
2017-08-27 11:24:51 -0700

    Finished simple calculator

---
+++ Simple Calculator/Default.aspx
@@ -0,0 +1,38 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Simple_Calculator.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <h1>Simple Calculator</h1>
+        <p>
+            First Value&nbsp;
+            <asp:TextBox ID="textboxFirstValue" runat="server"></asp:TextBox>
+        </p>
+        <p>
+            Second Value&nbsp;
+            <asp:TextBox ID="textboxSecondValue" runat="server"></asp:TextBox>
+        </p>
+        <p>
+&nbsp;<asp:Button ID="buttonPlus" runat="server" OnClick="buttonPlus_Click" Text="+" />
+&nbsp;
+            <asp:Button ID="buttonMinus" runat="server" OnClick="ButtonMinus_Click" Text="-" />
+&nbsp;
+            <asp:Button ID="buttonMultiply" runat="server" OnClick="buttonMultiply_Click" Text="*" />
+&nbsp;
+            <asp:Button ID="buttonDivide" runat="server" OnClick="buttonDivide_Click" Text="/" />
+&nbsp;</p>
+        <p>
+            <asp:Label ID="answerLabel" runat="server"></asp:Label>
+        </p>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ Simple Calculator/Default.aspx.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Simple_Calculator
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void buttonPlus_Click(object sender, EventArgs e)
+        {
+            double firstValue = double.Parse(textboxFirstValue.Text);
+            double secondValue = double.Parse(textboxSecondValue.Text);
+
+            double answer = firstValue + secondValue;
+
+            answerLabel.Text = answer.ToString();
+        }
+
+        protected void ButtonMinus_Click(object sender, EventArgs e)
+        {
+            double firstValue = double.Parse(textboxFirstValue.Text);
+            double secondValue = double.Parse(textboxSecondValue.Text);
+
+            double answer = firstValue - secondValue;
+
+            answerLabel.Text = answer.ToString();
+        }
+
+        protected void buttonMultiply_Click(object sender, EventArgs e)
+        {
+            double firstValue = double.Parse(textboxFirstValue.Text);
+            double secondValue = double.Parse(textboxSecondValue.Text);
+
+            double answer = firstValue * secondValue;
+
+            answerLabel.Text = answer.ToString();
+        }
+
+        protected void buttonDivide_Click(object sender, EventArgs e)
+        {
+            double firstValue = double.Parse(textboxFirstValue.Text);
+            double secondValue = double.Parse(textboxSecondValue.Text);
+
+            double answer = firstValue / secondValue;
+
+            answerLabel.Text = answer.ToString();
+        }
+    }
+}
